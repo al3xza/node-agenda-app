9707d661da53108d8b68286458be4d604cc2d5e1
al3xza <zaharia.ale@gmail.com>
2019-01-07 21:35:32 +0200

    load contacts

---
+++ public/javascripts/script.js
@@ -0,0 +1,22 @@
+function loadContacts(){
+    $.ajax('contacts.json').done(function (contacts) {
+displayContacts(contacts);
+    })
+}
+
+function displayContacts(contacts){
+    var rows = contacts.map(function(contact) {
+        console.log(contact);
+        return `<tr>
+        <td>${contact.firstName}</td>
+        <td>${contact.lastName}</td>
+        <td>${contact.phoneNumber}</td>
+        <td><a href="contacts.json?delete=${contact.phoneNumber}">X</a></td>
+        </tr>`;
+    });
+    console.warn(rows);
+    document.querySelector('tbody').innerHTML = rows.join('');
+}
+
+// - start app
+loadContacts();
